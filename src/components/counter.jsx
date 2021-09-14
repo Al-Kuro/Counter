9185ebfe328f050ac9172e24a0d66f9a79afe9aa
Al-Kuro <alx.kmnt@gmail.com>
2021-09-14 12:58:29 +0300

    Update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const formCount = () => {
-    return count === 0 ? "Ноль" : count;
+const Counter = ({ id, value, name, onIncrement, onDecrement, onDelete }) => {
+  const formValue = () => {
+    return value === 0 ? "Ноль" : value;
   };
 
   const getBadgeClasses = () => {
     let classes = "badge m-2 bg-";
-    classes += count === 0 ? "danger" : "primary";
+    classes += value === 0 ? "danger" : "primary";
     return classes;
-  };
-
-  const handleIncrement = (props) => {
-    console.log("propsIncrement", props);
-    setCount(count + 1);
-  };
-
-  const handleDecrement = (props) => {
-    console.log("propsDecrement", props);
-    return count === 0 ? "Ноль" : setCount(count - 1);
   };
 
   return (
     <>
-      <span className={getBadgeClasses()}>{formCount()}</span>
+      <h1>{name}</h1>
+      <span className={getBadgeClasses()}>{formValue()}</span>
       <button
-        onClick={() => handleIncrement({ id: 1 })}
+        onClick={() => onIncrement(id)}
         className="btn btn-secondary btn-sm"
       >
         Increment
       </button>
       <button
-        onClick={() => handleDecrement({ id: 1 })}
+        onClick={() => onDecrement(id)}
         className="btn btn-secondary btn-sm"
       >
         Decrement
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => onDelete(id)}
+      >
+        delete
       </button>
     </>
   );
